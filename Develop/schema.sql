1b5003bdc5145f8e5eb89cb3f37758097127f92d
NRocket <ninamrocket@gmail.com>
2020-06-28 16:44:28 -0700

    schema and seeds updated

--- Develop/schema.sql
+++ Develop/schema.sql
@@ -4,30 +4,33 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+ CREATE TABLE role
+(
+        id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
 CREATE TABLE employee
 (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY(id),
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
- CREATE TABLE role
-(
-        id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR (30),
-    salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
-);
 
-CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30),
-    PRIMARY KEY(id)
-);
+
--- Develop/seeds.sql
+++ Develop/seeds.sql
@@ -1,20 +1,28 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES ("Harry", "Henderson", 1, 23),
-("Susan", "Apple", 1, 23),
-("Melinda", "Reed", 1, 23),
-("Olivia", "August", 1, 23),
-("Hillary", "Clinton", 1, 23),
-("Boston", "Dex", 1, 23),
-("Alice", "Wilkins", 2, 12),
-("Hannah", "Musk", 2, 12),
-("Octavian", "Smith", 2, 12);
+USE employee_db; 
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Harmony", "Blake", 23), ("Rosco", "Asante", 12);
+INSERT INTO department (department_name)
+VALUES ("Enrollment"), ("Financial Aid");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Enrollment Advisor", 53000, 1), ("Financial Aid Advisor", 49000, 2);
 
-INSERT INTO department (department_name)
-VALUES ("Enrollment"), ("Financial Aid");
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Harmony", "Blake", 1), ("Rosco", "Asante", 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ("Harry", "Henderson", 1, 1),
+("Susan", "Apple", 1, 1),
+("Melinda", "Reed", 1, 1),
+("Olivia", "August", 1, 1),
+("Hillary", "Clinton", 1, 1),
+("Boston", "Dex", 1, 1),
+("Alice", "Wilkins", 2, 2),
+("Hannah", "Musk", 2, 2),
+("Octavian", "Smith", 2, 2);
+
+
+
+
+
+
 
