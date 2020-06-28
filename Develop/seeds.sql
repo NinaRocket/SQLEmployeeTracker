0caae86e658d0bd0d98158ebef024527b48e0c70
NRocket <ninamrocket@gmail.com>
2020-06-28 16:12:01 -0700

    seeds and schema complete

---
+++ Develop/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ("Harry", "Henderson", 1, 23),
+("Susan", "Apple", 1, 23),
+("Melinda", "Reed", 1, 23),
+("Olivia", "August", 1, 23),
+("Hillary", "Clinton", 1, 23),
+("Boston", "Dex", 1, 23),
+("Alice", "Wilkins", 2, 12),
+("Hannah", "Musk", 2, 12),
+("Octavian", "Smith", 2, 12);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Harmony", "Blake", 23), ("Rosco", "Asante", 12);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Enrollment Advisor", 53000, 1), ("Financial Aid Advisor", 49000, 2);
+
+INSERT INTO department (department_name)
+VALUES ("Enrollment"), ("Financial Aid");
+
